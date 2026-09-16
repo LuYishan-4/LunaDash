d1c91b7a64724a9ac57c19eb70921528ca7a09e6
LuYishan <wuming381906@gmail.com>
2026-09-17 05:31:46 +0800

    files: add terminal icon

--- include/LuDash/file_icons/FileIcons.h
+++ include/LuDash/file_icons/FileIcons.h
@@ -2,6 +2,6 @@
 #include <QIcon>
 #include <QColor>
 namespace LuDash {
-enum class FileIcon { Home, Folder, File, Desktop, Download, Picture, Music, Video, Drive, Back, Forward, Up, Grid, List, Plus, Copy, Cut, Paste, Rename, Trash, Search, Eye };
+enum class FileIcon { Home, Folder, File, Desktop, Download, Picture, Music, Video, Drive, Back, Forward, Up, Grid, List, Plus, Copy, Cut, Paste, Rename, Trash, Search, Eye, Terminal };
 QIcon fileIcon(FileIcon icon, const QColor& color = QColor("#aebccc"));
 }
