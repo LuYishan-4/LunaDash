2720a199ba7aff9ef7b3d4873b608b774eefd20d
LuYishan <wuming381906@gmail.com>
2026-09-17 12:33:32 +0800

    i18n: expose supported locale detection

--- include/LuDash/localization/Localization.h
+++ include/LuDash/localization/Localization.h
@@ -5,6 +5,7 @@ class QCoreApplication;
 namespace LuDash {
 QString translate(const char* source);
 QString selectedLanguage();
+bool isSupportedLanguage(const QString& language);
 QJsonObject languageDictionary(const QString& language);
 void initializeLocalization(QCoreApplication& application);
 }
