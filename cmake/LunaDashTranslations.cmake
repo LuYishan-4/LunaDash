4809ea79af13912c6a557e720303324ab82a4df9
LuYishan <wuming381906@gmail.com>
2026-09-17 10:09:57 +0800

    translations: include plugin manager catalog

--- cmake/LunaDashTranslations.cmake
+++ cmake/LunaDashTranslations.cmake
@@ -7,6 +7,7 @@ qt_add_resources(ludash-localization desktop_translations
         ${CMAKE_CURRENT_LIST_DIR}/../data/translations/zh_TW/desktop.json
         ${CMAKE_CURRENT_LIST_DIR}/../data/translations/zh_TW/files.json
         ${CMAKE_CURRENT_LIST_DIR}/../data/translations/zh_TW/settings.json
+        ${CMAKE_CURRENT_LIST_DIR}/../data/translations/zh_TW/plugins.json
 )
 if(TARGET ludash-file-operations)
     target_link_libraries(ludash-file-operations PUBLIC ludash-localization)
