a6acd9e517014915c4cf8ad56d4a428c2662d850
LuYishan <wuming381906@gmail.com>
2026-09-17 11:27:34 +0800

    translations: compile network and device catalogue

--- cmake/LunaDashTranslations.cmake
+++ cmake/LunaDashTranslations.cmake
@@ -8,6 +8,7 @@ qt_add_resources(ludash-localization desktop_translations
         ${CMAKE_CURRENT_LIST_DIR}/../data/translations/zh_TW/files.json
         ${CMAKE_CURRENT_LIST_DIR}/../data/translations/zh_TW/settings.json
         ${CMAKE_CURRENT_LIST_DIR}/../data/translations/zh_TW/plugins.json
+        ${CMAKE_CURRENT_LIST_DIR}/../data/translations/zh_TW/network_devices.json
 )
 if(TARGET ludash-file-operations)
     target_link_libraries(ludash-file-operations PUBLIC ludash-localization)
