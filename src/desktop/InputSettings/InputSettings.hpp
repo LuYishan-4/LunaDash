ba5198aa33aeec0727da39450f85170d38b8b92a
LuYishan <wuming381906@gmail.com>
2026-09-18 08:00:55 +0800

    Expose lock LED synchronization hooks

--- src/desktop/InputSettings/InputSettings.hpp
+++ src/desktop/InputSettings/InputSettings.hpp
@@ -2,5 +2,7 @@
 #include <QJsonObject>
 class QWaylandSeat;
 namespace LuDash {
+void setKeyboardLedControlEnabled(bool enabled);
+void handleKeyboardLockKey(int qtKey, bool pressed, bool autoRepeat);
 void applyKeyboardPreferences(QWaylandSeat *seat, const QJsonObject &preferences);
 }
