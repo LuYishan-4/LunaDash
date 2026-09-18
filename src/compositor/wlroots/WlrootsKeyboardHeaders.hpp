8818882133968107f2888a61830dcdc74dc99a81
LuYishan <wuming381906@gmail.com>
2026-09-18 17:52:10 +0800

    Split wlroots keyboard header boundary

---
+++ src/compositor/wlroots/WlrootsKeyboardHeaders.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+#define static
+extern "C" {
+#endif
+
+#include <wlr/types/wlr_keyboard.h>
+#include <xkbcommon/xkbcommon.h>
+
+#ifdef __cplusplus
+}
+#undef static
+#endif
