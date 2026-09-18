8b1571af3f2f1df3de7324a0999ec08a954b5bde
LuYishan <wuming381906@gmail.com>
2026-09-18 15:33:43 +0800

    Expose OpenGL shader stage metadata

--- src/compositor/render/RenderBackend/RenderBackend.hpp
+++ src/compositor/render/RenderBackend/RenderBackend.hpp
@@ -5,8 +5,21 @@
 #include <atomic>
 #include <memory>
 #include <optional>
+
 namespace LuDash {
+
 enum class GraphicsApi { Auto, OpenGL, OpenGLES };
+
+enum class ShaderStage {
+    Unknown,
+    Vertex,
+    Fragment,
+    Geometry,
+    Compute,
+    TessControl,
+    TessEvaluation,
+};
+
 struct RenderState {
     std::atomic<bool> shaderReady{false};
     std::atomic<bool> failed{false};
@@ -14,8 +27,13 @@ struct RenderState {
     std::atomic<int> majorVersion{0};
     std::atomic<int> minorVersion{0};
 };
+
 std::optional<GraphicsApi> graphicsApiFromArguments(int argc, char** argv);
 void configureGraphics(GraphicsApi api);
 LuDashGLProc resolveGLFunction(const char* name);
+
+ShaderStage shaderStageFromName(const QString& name);
+GLenum shaderStageGlEnum(ShaderStage stage);
 QByteArray shaderSource(const QString& name, bool openGLES);
+
 }
