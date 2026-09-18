b52837e99094b81bfad51ded196aa69191fac691
LuYishan <wuming381906@gmail.com>
2026-09-18 14:37:49 +0800

    Define compositor-side input method bridge API

--- src/compositor/input/InputMethodSupport/InputMethodSupport.hpp
+++ src/compositor/input/InputMethodSupport/InputMethodSupport.hpp
@@ -1,5 +1,32 @@
 #pragma once
+
+#include <QObject>
+#include <memory>
+
+class QKeyEvent;
+class QQuickWindow;
 class QWaylandCompositor;
+class QWaylandOutput;
+
 namespace LuDash {
-void installInputMethodProtocols(QWaylandCompositor* compositor);
-}
+
+class InputMethodSupport final : public QObject {
+public:
+  InputMethodSupport(QWaylandCompositor *compositor, QQuickWindow *window,
+                     QWaylandOutput *output);
+  ~InputMethodSupport() override;
+
+  bool filterKeyEvent(QKeyEvent *event);
+  bool hasKeyboardGrab() const;
+  bool nativeBridgeAvailable() const;
+
+private:
+  class Impl;
+  std::unique_ptr<Impl> d;
+};
+
+InputMethodSupport *installInputMethodProtocols(QWaylandCompositor *compositor,
+                                                QQuickWindow *window,
+                                                QWaylandOutput *output);
+
+} // namespace LuDash
