36324e14e151a60c4967f5bf9b082314ff50e905
LuYishan <wuming381906@gmail.com>
2026-09-18 10:56:37 +0800

    Model niri-style window opening policy

--- src/compositor/WindowRules/WindowRules.hpp
+++ src/compositor/WindowRules/WindowRules.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <QSize>
 #include <QString>
 
 namespace LuDash {
 
 struct InitialWindowPolicy {
+  // Match niri's opening semantics: ordinary windows start tiled at the
+  // configured default column width and are not maximized implicitly.
   bool maximized = false;
+  bool floating = false;
+  QSize floatingSize;
 };
 
 InitialWindowPolicy initialWindowPolicy(const QString &appId,
