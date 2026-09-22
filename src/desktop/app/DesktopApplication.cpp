5dae3e5ef71e1efd326ded8931416998b1965f5b
LuYishan <wuming381906@gmail.com>
2026-09-22 10:20:16 +0800

    refactor(desktop): remove legacy widget launcher catalog

--- src/desktop/app/DesktopApplication.cpp
+++ src/desktop/app/DesktopApplication.cpp
@@ -1,13 +1,11 @@
 #include "desktop/app/DesktopApplication.hpp"
 #include "config/BuildConfig.hpp"
 #include "config/localization/Localization.hpp"
-#include "desktop/app/ApplicationCatalog.hpp"
 #include "desktop/app/ApplicationWindow.hpp"
 #include "desktop/app/DefaultApplications.hpp"
 #include "desktop/app/WaylandClientShutdown.hpp"
 #include "desktop/console/Console.hpp"
 #include "desktop/filemanager/FileManager.hpp"
-#include "desktop/launcher/Launcher.hpp"
 #include "desktop/package/PackageManager.hpp"
 #include "desktop/theme/DesktopTheme.hpp"
 #include "desktop/welcome/Welcome.hpp"
@@ -79,8 +77,6 @@ int DesktopApplication::run(int argc, char **argv) {
       content = LunaDash::createConsole();
     else if (id == "packages")
       content = LunaDash::createPackageManager();
-    else if (id == "launcher")
-      content = LunaDash::createLauncher();
     else if (id == "welcome")
       content = LunaDash::createWelcome([](const QString &target) {
         QProcess::startDetached(QCoreApplication::applicationFilePath(),
