42df979b0d07aac0edd55e5b57b3666a69807037
LuYishan <wuming381906@gmail.com>
2026-09-26 13:04:40 +0800

    perf(network): listen for NetworkManager changes

--- src/desktop/network/NetworkStatus.hpp
+++ src/desktop/network/NetworkStatus.hpp
@@ -3,13 +3,18 @@
 #include <QJsonObject>
 #include <QObject>
 #include <QString>
+class QTimer;
 namespace LunaDash {
 QJsonObject describeNetwork(unsigned int state, unsigned int connectivity);
 class NetworkStatus final : public QObject {
+  Q_OBJECT
 public:
   explicit NetworkStatus(QObject *parent = nullptr);
   QJsonObject snapshot() const;
   bool execute(const QJsonObject &request, QString *error);
+
+private Q_SLOTS:
+  void scheduleRefresh();
 
 private:
   void refresh();
@@ -18,6 +23,7 @@ private:
   bool pending_ = false;
   CommandRunner *command_ = nullptr;
   CommandRunner *bluetoothCommand_ = nullptr;
+  QTimer *eventRefresh_ = nullptr;
   QString nmcli_;
   QString bluetoothctl_;
 };
