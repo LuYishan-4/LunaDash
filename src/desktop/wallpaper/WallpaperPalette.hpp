f89dfb203475dc7fc2eb5b3d6ec6b861cfebdc5c
LuYishan <wuming381906@gmail.com>
2026-09-26 14:38:58 +0800

    perf(wallpaper): expose optimized playback path

--- src/desktop/wallpaper/WallpaperPalette.hpp
+++ src/desktop/wallpaper/WallpaperPalette.hpp
@@ -5,5 +5,6 @@
 namespace LunaDash {
 void refreshWallpaperPalette(const QString &path, bool video);
 QString wallpaperPoster(const QString &path);
+QString wallpaperPlaybackPath(const QString &path);
 QJsonObject wallpaperMediaStatus();
 } // namespace LunaDash
